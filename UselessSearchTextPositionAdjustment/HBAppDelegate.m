fad67a7571706853b8b7c67423947d92a0221d1c
Heath Borders <heath.borders@jivesoftware.com>
2013-06-20 08:59:48 -0500

    Demonstrates rdar://13963273.
    UISearchBar.searchTextPositionAdjustment doesn't scroll search text properly.

--- UselessSearchTextPositionAdjustment/HBAppDelegate.m
+++ UselessSearchTextPositionAdjustment/HBAppDelegate.m
@@ -15,35 +15,24 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    UIViewController *viewController = [UIViewController new];
+    
+    UISearchBar *searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(50, 50, 200, 100)];
+    searchBar.searchTextPositionAdjustment = UIOffsetMake(100,
+                                                          0);
+    [viewController.view addSubview:searchBar];
+    
+    UILabel *directionsLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 200, 250, 200)];
+    directionsLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    directionsLabel.numberOfLines = 0;
+    directionsLabel.text = @"Start typing in the above UISearchBar. Notice how the text overflows the clear button and then clips before horizontally scrolling";
+    [viewController.view addSubview:directionsLabel];
+    
+    self.window.rootViewController = viewController;
+    
     [self.window makeKeyAndVisible];
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
