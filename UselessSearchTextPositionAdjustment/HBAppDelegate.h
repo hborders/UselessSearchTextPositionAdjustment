475087c39359eaa7ae08f09acf411ccdcf81c35d
Heath Borders <heath.borders@jivesoftware.com>
2013-06-20 08:52:28 -0500

    Initial Commit

---
+++ UselessSearchTextPositionAdjustment/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  UselessSearchTextPositionAdjustment
+//
+//  Created by Heath Borders on 6/20/13.
+//  Copyright (c) 2013 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UselessSearchTextPositionAdjustment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UselessSearchTextPositionAdjustment
+//
+//  Created by Heath Borders on 6/20/13.
+//  Copyright (c) 2013 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HBAppDelegate class]));
+    }
+}
